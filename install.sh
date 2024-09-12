36648e8de1f23e765b4e63d3cff30ca328eda3a9
jrollio <75393433+jrollio@users.noreply.github.com>
2024-09-11 21:23:15 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
-for f in $( /bin/ls -a ./.* ); do [[ -f $HOME/${f} && -w $HOME/${f} ]] && cp ${HOME}/${f} ./${f}.bkp; done;
+
+exec > /dev/null 2>&1
+
+for f in $( /bin/ls -a ./.* ); do
+	[[ -f $HOME/${f} && -w $HOME/${f} ]] && cp ${HOME}/${f} ./${f}.bkp;
+	cp ${f} $HOME;
+done
 
 cd $HOME
 
 [[ ! $( echo $0 | grep "bash" ) ]] && ENTRYPOINT=.bashrc || ENTRYPOINT=.profile
 
 echo "cd $HOME; . ./.env" >> ./$ENTRYPOINT
-
-for f in $( /bin/ls -a ./dots/.*); do [[ -f $f} ]] && cp ${f} ./; done;
